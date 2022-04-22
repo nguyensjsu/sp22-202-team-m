e21c7965adb8b631a7039ed497b59a9af89028fa
ankitajaswal <ankitajaswal508@gmail.com>
2022-04-21 21:37:54 -0700

    add level1 feature

---
+++ Snake - Blank Student/Level1.java
@@ -0,0 +1,40 @@
+
+/**
+ * The Class Level1.
+ */
+public class Level1 implements ILevelState {
+
+    LevelState l;
+    int speed;
+
+    /**
+     * Instantiates a new level 1.
+     *
+     * @param l the l
+     */
+    public Level1(LevelState l) {
+        
+        this.speed = 20;
+        this.l = l;
+    }
+
+    /**
+     * Change state.
+     */
+    @Override
+    public void changeState() {
+
+        l.changeToLevel2();
+    }
+    
+    /**
+     * Gets the current speed.
+     *
+     * @return the level speed
+     */
+    public int getSpeed(){
+        return speed;
+    }
+
+}
+
