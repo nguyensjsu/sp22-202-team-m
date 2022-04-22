a7a5ff4f35847bbe0a06524bb6f6693d4b226cd9
ankitajaswal <ankitajaswal508@gmail.com>
2022-04-21 21:34:49 -0700

    add Score subject interface

---
+++ Snake - Blank Student/IScoreSubject.java
@@ -0,0 +1,27 @@
+/**
+ * Write a description of class IScoreSubject here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface IScoreSubject  
+{
+   
+    /**
+     * attach observer.
+     *
+     * @param observer the observer
+     */
+    public void attachObserver(IScoreObserver observer);
+    
+    /**
+     * Remove Observer
+     * @param obj Pin AUth Observer to Remove
+     */
+    void removeObserver( IScoreObserver obj ) ;
+    
+    /**
+     * Notify observer.
+     */
+    public void notifyLevelState();
+}
