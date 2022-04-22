299ea16ce66859df8c065b05493b71f478b5daf9
ankitajaswal <ankitajaswal508@gmail.com>
2022-04-21 21:38:29 -0700

    add level3 feature

---
+++ Snake - Blank Student/Level3.java
@@ -0,0 +1,42 @@
+import greenfoot.*;
+
+/**
+ * The Class Level3 implements Level State to create a new level 3
+ */
+public class Level3 implements ILevelState {
+
+    LevelState l;
+    int speed;
+
+    /**
+     * Creates Level 3
+     *
+     * @param l
+     */
+    public Level3(LevelState l) {
+        
+        this.speed = 40;
+        this.l = l;
+    }
+
+    /**
+     * Change state.
+     */
+    @Override
+    public void changeState() {
+        l.changeToLevel4();
+        
+    }
+
+    /**
+     * Gets the current speed.
+     *
+     * @return the level speed
+     */
+    public int getSpeed(){
+        return speed;
+    }
+}
+
+
+
