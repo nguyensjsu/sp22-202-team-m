1194a23b7bd16f42f00744868119f20599b3097c
ankitajaswal <ankitajaswal508@gmail.com>
2022-04-21 21:35:11 -0700

    add Score observer interface

---
+++ Snake - Blank Student/IScoreObserver.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class IScoreObserver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface IScoreObserver 
+{
+   void changeState(int score);
+}
