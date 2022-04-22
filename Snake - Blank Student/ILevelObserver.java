311f99cb3d538cac063343e4bdbbb2c322898af8
ankitajaswal <ankitajaswal508@gmail.com>
2022-04-21 21:46:00 -0700

    add level state machine - observer

---
+++ Snake - Blank Student/ILevelObserver.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class ILevelObserver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface ILevelObserver  
+{
+    
+    /**
+     * update speed each level
+     * @param speed the speed
+     */
+    public void update(int speed);
+}
+
