5dc08a0dbc950fc4ab35c82064b774f581121af9
ankitajaswal <ankitajaswal508@gmail.com>
2022-04-21 21:38:11 -0700

    add level2 feature

---
+++ Snake - Blank Student/Level2.java
@@ -0,0 +1,41 @@
+import greenfoot.*;
+
+/**
+ * The Class Level2 implements Level State to create a new level 2
+ */
+public class Level2 implements ILevelState {
+
+    LevelState l;
+    int speed;
+
+    /**
+     * Creates Level 2
+     *
+     * @param l
+     */
+    public Level2(LevelState l) {
+        
+        this.speed = 30;
+        this.l = l;
+    }
+
+    /**
+     * Change state.
+     */
+    @Override
+    public void changeState() {
+        l.changeToLevel3();
+           //l.endGame();    
+    }
+
+    /**
+     * Gets the current speed.
+     *
+     * @return the level speed
+     */
+    public int getSpeed(){
+        return speed;
+    }
+}
+
+
