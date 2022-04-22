231e964b20b9769c8abe9f468a54c92bb22f7294
ankitajaswal <ankitajaswal508@gmail.com>
2022-04-21 21:39:01 -0700

    add level5 feature

---
+++ Snake - Blank Student/Level5.java
@@ -0,0 +1,42 @@
+import greenfoot.*;
+
+/**
+ * The Class Level5 implements Level State to create a new level 5
+ */
+public class Level5 implements ILevelState {
+
+    LevelState l;
+    int speed;
+
+    /**
+     * Creates Level 5
+     *
+     * @param l
+     */
+    public Level5(LevelState l) {
+        
+        this.speed = 40;
+        this.l = l;
+    }
+
+    /**
+     * Change state.
+     */
+    @Override
+    public void changeState() {
+        l.endGame();
+        
+    }
+
+    /**
+     * Gets the current speed.
+     *
+     * @return the level speed
+     */
+    public int getSpeed(){
+        return speed;
+    }
+}
+
+
+
